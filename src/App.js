4fa84d5a9064d4d4e9e59ff4dc90c8a1ba5e981b
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-10-03 22:46:42 -0400

    some progress on state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
+import {Component} from 'react';
 import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: {firstName: 'Maxter' , lastName: 'Moro'},
+      city: 'Toronto'
+    };
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hi {this.state.name.firstName} {this.state.name.lastName}, we live in {this.state.city}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button
+         onClick={() => {
+          this.setState((state, props) => { 
+            return {
+            name: {firstName:'Stasic', lastName: 'Morozan'},
+          };
+          },
+        () => {
+
+        }
+        );
+          
+        }}>
+        Change the Name
+        </button>
       </header>
     </div>
   );
+}
 }
 
 export default App;
