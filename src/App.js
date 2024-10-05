46225333e165a1f33c939277fdbc088ad7899c11
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-10-04 23:12:57 -0400

    more progress on the app functionals

--- src/App.js
+++ src/App.js
@@ -1,44 +1,57 @@
-import {Component} from 'react';
+import { Component } from 'react';
 import logo from './logo.svg';
 
 import './App.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      name: {firstName: 'Maxter' , lastName: 'Moro'},
-      city: 'Toronto'
+      members: [],
     };
   }
 
-  render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hi {this.state.name.firstName} {this.state.name.lastName}, we live in {this.state.city}
-        </p>
-        <button
-         onClick={() => {
-          this.setState((state, props) => { 
-            return {
-            name: {firstName:'Stasic', lastName: 'Morozan'},
-          };
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { members: users };
           },
-        () => {
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
 
-        }
-        );
+  render() {
+    return (
+      <div className="App">
+        <input className='search-box' type='search' placeholder='search members' onChange={(event) => {
+          const searchString = event.target.value.toLocaleLowerCase();
+          const filteredMembers = this.state.members.filter((member)=>{
+           return member.name.toLocaleLowerCase().includes(searchString);
+          });
           
-        }}>
-        Change the Name
-        </button>
-      </header>
-    </div>
-  );
-}
+          this.setState(() => {
+            return {members:filteredMembers};
+          } 
+        );
+
+        }} 
+        />
+        {this.state.members.map((member) => {
+          return (
+            <div key={member.id}>
+              <h1>{member.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
