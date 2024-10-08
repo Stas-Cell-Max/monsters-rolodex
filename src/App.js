cc695625a1eea063d3f49ea07fb2a7fe998ee997
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-10-07 23:05:17 -0400

    added styling and members avatars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import logo from './logo.svg';
 import CardList from './components/card-list/card-list.component'
+import SearchBox from './components/search-box/search-box.component';
 
 import './App.css';
 
@@ -16,15 +17,9 @@ class App extends Component {
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { members: users };
-          },
-          
-        )
-      );
+      .then((users) => this.setState({ members: users }));
   }
+ 
   onSearchChange = (event) => {
     const searchField = event.target.value.toLocaleLowerCase();
     this.setState(() => {
@@ -36,27 +31,15 @@ class App extends Component {
 
     const { members, searchField,} = this.state;
     const {onSearchChange} = this;
+
     const filteredMembers = members.filter((member)=>{
       return member.name.toLocaleLowerCase().includes(searchField);
      });
+
     return (
       <div className="App">
-        <input className='search-box' 
-        type='search' 
-        placeholder='search members' 
-        onChange={onSearchChange}
-        
-        
-        
-        />
-          { /*filteredMembers.map((member) => {
-          return (
-            <div key={member.id}>
-              <h1>{member.name}</h1>
-            </div>
-          );
-        })
-        */}
+      
+        <SearchBox className = 'members-search-box' onChangeHandler={onSearchChange} placeholder = 'search members' />
         <CardList members={filteredMembers} />
       </div>
     );
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,17 +1,25 @@
-import { Component } from "react";
+import { Component } from 'react';
+import './card-list.styles.css';
 
 
 class CardList extends Component {
 
     render () {
-const {members} = this.props;
+        const {members} = this.props;
 
-        return <div>
+        return (
+        <div className = 'card-list' > 
             {members.map((member) => (
-            <h1 key={member.id} > {member.name}</h1>
-            )
-            )}
+            <div className= 'card-container' key={member.id} > 
+            <img alt={`member ${member.name}`} src={`https://robohash.org/${member.id}?set=set2&size=160x160`} />
+            <h2>{member.name}</h2>
+            <p>{member.email}</p>
+            
+            {member.name}
             </div>
+            ))}
+        </div>
+        );
     }
 }
 export default CardList;
